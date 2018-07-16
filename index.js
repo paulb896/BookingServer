867ccfb4ae69967a668275031cc18dfdefb862de
Paul Beauchamp <paulb896@gmail.com>
2018-07-16 00:04:27 -0700

    Save company data instead of user data

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 const axios = require('axios');
-// const session = require('koa-session');
 const session  = require('koa-generic-session')
 const redisStore = require('koa-redis');
-const redis = require("redis");
+const redis = require('redis');
 const redisClient = redis.createClient();
 
 // Initialize App and router
@@ -13,13 +12,25 @@ const router = new Router();
 const storage = redisStore({
     url: 'redis://localhost:6379'
 });
-
 app.keys = ['keys-this-is-a-test', 'keykeys'];
-
 app.use(session({
     store: storage
 }, app));
+app.use(router.routes());
+app.use(router.allowedMethods());
 
+// Set CORS HEADERS
+app.use(async (ctx, next) => {
+    ctx.set('Access-Control-Allow-Origin', '*');
+    ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    ctx.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    await next();
+});
+
+/**
+ * Public View counter endpoint that counts view for all users total,
+ * and for the current user in their session.
+ */
 router.get('/view-counter', async (ctx, next) => {
     let userSpecificViews = ctx.session.userSpecificViews || 0;
 
@@ -41,47 +52,45 @@ router.get('/view-counter', async (ctx, next) => {
     redisClient.incr('totalViews');
 });
 
+/**
+ * Company Booking Endpoint that returns company details
+ * and all bookings for that company.
+ */
 router.get('/company/:id', async (ctx, next) => {
     const userId = 1;
-    const companyId = 1;
+    const companyId = ctx.params.id;
     const companyCacheKey = `user-${userId}-company-${companyId}`;
 
-    await new Promise( async (resolve, reject) => {
-        redisClient.get(companyCacheKey, async (err, companyData) => {
+    await next();
+    await new Promise((resolve, reject) => {
+        redisClient.get(companyCacheKey, (err, companyData) => {
             if (err || !companyData) {
                 let fullCompany = {message: 'no data'};
-                const user = axios(`http://localhost:4000/users/${userId}`);
+                const company = axios(`http://localhost:4000/companies/${companyId}`);
                 const booking = axios(`http://localhost:4000/bookings?company=${companyId}`);
 
-                Promise.all([user, booking]).then((responses) => {
-                    fullCompany = {
-                        user: responses[0].data,
-                        bookings: responses[1].data,
-                    };
-                    ctx.body = fullCompany;
-                    redisClient.set(companyCacheKey, JSON.stringify(fullCompany));
-                    resolve();
+                Promise.all([company, booking]).then((responses) => {
+                    if (responses.length === 2) {
+                        fullCompany = {
+                            company: responses[0].data,
+                            bookings: responses[1].data,
+                        };
+                        ctx.body = fullCompany;
+                        redisClient.set(companyCacheKey, JSON.stringify(fullCompany));
+                        resolve();
+                    } else {
+                        reject();
+                    }
                 })
             } else {
                 ctx.body = JSON.parse(companyData);
                 resolve();
             }
         });
-    });
+    })
 
     redisClient.incr('totalViews');
 })
-
-// Setup Router
-app
-  .use(router.routes())
-  .use(router.allowedMethods());
-
-// Set CORS HEADERS
-app.use(function *(){
-    this.set('Access-Control-Allow-Origin', '*');
-    this.set('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
-});
 
 app.listen(3000);
 app.use(async (ctx, next) => {
